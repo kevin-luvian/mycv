a77ff39e2403399c81b5450f193070343103fe48
kevin-luvian <kevinlh90@gmail.com>
2022-01-05 13:27:32 +0700

    feat: hash loader spinner

--- frontend/src/component/loader/hash.js
+++ frontend/src/component/loader/hash.js
@@ -1,14 +1,9 @@
 import styles from "./styles.module.scss";
 import Loader from "react-spinners/HashLoader";
-import { Fragment } from "react";
 
-const HashLoader = ({ loading }) => {
-    if (loading) return (
-        <div className={styles.loader}>
-            <Loader css={`position: inherit;`} size={70} color='#0973d6' />
-        </div>
-    )
-    return <Fragment />
-}
+const HashLoader = () =>
+    <div className={styles.loader}>
+        <Loader css={`position: inherit;`} size={70} color='#0973d6' />
+    </div>
 
 export default HashLoader;
