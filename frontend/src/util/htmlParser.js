bbaa57c1e7fa108a4a56326afb29971845d540c3
kevin-luvian <kevinlh90@gmail.com>
2022-01-17 10:05:12 +0700

    feat: parse html string using components

--- frontend/src/util/htmlParser.js
+++ frontend/src/util/htmlParser.js
@@ -1,37 +1,42 @@
 import { DomElement } from "htmlparser2";
-import ReactHtmlParser, { convertNodeToElement } from 'react-html-parser';
-import { cnord, concat } from "./utils";
-import styles from "./styles.module.scss";
+import ReactHtmlParser, { convertNodeToElement } from "react-html-parser";
+import { cnord } from "./utils";
+import { Basic } from "../component/button/Button";
+import { Heading } from "../component/texts/Heading";
+import { Tips } from "../component/texts/InfoBar";
 
 export const parse = (s) => {
   try {
-    const parsed = ReactHtmlParser(s, {
-      transform: transformButton
-    });
-    console.log(parsed);
+    const parsed = ReactHtmlParser(s, { transform });
     return parsed;
   } catch (err) {
-    return <p> error, cant parse html content </p>
+    return <p> error, cant parse html content </p>;
   }
-}
-
-/**
- * @param {DomElement} node 
- */
-const transform = (node, index) => {
-  transformButton(node);
-  return convertNodeToElement(node, index, transform);
-}
+};
 
 /**
  * @param {DomElement} node
  */
-const transformButton = (node) => {
+const transform = (node, index) => {
   node.attribs = cnord(node.attribs, {});
+  const props = {
+    key: index,
+    className: node.attribs["class"],
+  };
   switch (node.name) {
     case "primary-button":
-      node.name = "button";
-      node.attribs["class"] = concat(node.attribs["class"], styles.btn, styles.primary);
-      break;
+      return <Basic.Default {...props} children={convCH(node, index)} />;
+    case "heading":
+      return <Heading {...props} children={convCH(node, index)} />;
+    case "tips":
+      return <Tips {...props} children={convCH(node, index)} />;
   }
-}
+  return convertNodeToElement(node, index, transform);
+};
+
+/**
+ * @param {DomElement} node
+ */
+const convCH = (node, index) =>
+  node.children?.map((e) => convertNodeToElement(e, index + 1, transform)) ||
+  [];
