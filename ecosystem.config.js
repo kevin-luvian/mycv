0f1cf1451c2570a385c43e86913d5cdf0e3f48e5
kevin-luvian <kevinlh90@gmail.com>
2022-03-22 09:39:44 +0700

    refactor: change cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,37 +3,21 @@ module.exports = {
     {
       name: "[MYCV] main",
       script: "./backend/bin/www",
-      exec_mode: "fork",
+      instances: 2,
+      exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
         PORT: 9000,
       },
     },
     {
-      name: "[MYCV] file worker 1",
+      name: "[MYCV] file worker",
       script: "./backend/bin/www",
-      exec_mode: "fork",
+      instances: 2,
+      exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
         PORT: 9001,
-      },
-    },
-    {
-      name: "[MYCV] file worker 2",
-      script: "./backend/bin/www",
-      exec_mode: "fork",
-      env: {
-        NODE_ENV: "production",
-        PORT: 9002,
-      },
-    },
-    {
-      name: "[MYCV] file worker 3",
-      script: "./backend/bin/www",
-      exec_mode: "fork",
-      env: {
-        NODE_ENV: "production",
-        PORT: 9003,
       },
     },
   ],
