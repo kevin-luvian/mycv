6775028d5c3e00e380d8a15faa6fcea5501d96e2
kevin-luvian <54918671+kevin-luvian@users.noreply.github.com>
2022-12-09 07:56:26 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "[MYCV] main",
+      name: "[MYCV] main #1",
       script: "./backend/bin/www",
-      instances: 2,
+      instances: 1,
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
@@ -12,15 +12,38 @@ module.exports = {
       },
     },
     {
-      name: "[MYCV] file worker",
+      name: "[MYCV] main #2",
       script: "./backend/bin/www",
-      instances: 2,
+      instances: 1,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        API_URL: "https://mycv.atkev.tech/api",
+        PORT: 9001,
+      },
+    },
+    {
+      name: "[MYCV] file worker #1",
+      script: "./backend/bin/www",
+      instances: 1,
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
         MONGO_DB:
           "mongodb://root:password@localhost:27017/mongodb?authSource=admin&retryWrites=true&w=majority",
-        PORT: 9001,
+        PORT: 9002,
+      },
+    },
+    {
+      name: "[MYCV] file worker #2",
+      script: "./backend/bin/www",
+      instances: 1,
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        MONGO_DB:
+          "mongodb://root:password@localhost:27017/mongodb?authSource=admin&retryWrites=true&w=majority",
+        PORT: 9003,
       },
     },
   ],
